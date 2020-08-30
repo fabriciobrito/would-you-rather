042dfc1fb46596bf5f0269c14939530161bc436b
Fabricio Brito <fabricio.brito@gmail.com>
2020-08-29 21:10:09 -0300

    feat: Remove user logged in by default

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,10 +4,6 @@ import { setAuthedUser } from './authedUser';
 import { getInitialData } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
-//Temporary set authed user from existing questions on database
-//ToDo: remove default authed_user
-const AUTHED_ID = 'sarahedo';
-
 export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading);
@@ -15,7 +11,7 @@ export function handleInitialData() {
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users));
         dispatch(receiveQuestions(questions));
-        dispatch(setAuthedUser(AUTHED_ID));
+        dispatch(setAuthedUser(''));
         dispatch(hideLoading);
       })
   }
